3f6fd3aa749ae5302c76ba8a7a80cf79174f6dd8
alinemsiqueira <aline.mscruz1@gmail.com>
2021-07-22 14:50:59 -0300

    Adiciona arquivos .sh para cada uma das 4 partes

---
+++ bloco-01-unix-e-bash/dia-04-unix-e-bash-part-2/parte-1.sh
@@ -0,0 +1,27 @@
+# Exercício 1
+cd unix_tests
+
+# Exercício 2
+cat > skills2.txt
+# Internet, Unix e Bash
+
+# Exercício 3
+cat >> skills2.txt
+# SQL, React, HTML, CSS, JavaScript
+sort skills2.txt
+
+# Exercício 4
+wc -l skills2.txt
+
+# Exercício 5
+cat skills2 | sort > top_skills.txt
+
+# Exercício 6
+cat > phrases2.txt
+# A repetição é a chave para o aprendizado.
+# A sabedoria não vem do acerto, mas do aprendizado com os erros.
+# O aprendizado é conhecimento, e conhecimento é liberdade e poder.
+
+# Exercício 7
+
+
