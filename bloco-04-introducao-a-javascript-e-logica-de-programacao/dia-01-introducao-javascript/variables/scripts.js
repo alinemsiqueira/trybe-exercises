0866782bdf6ee068a99236b3ed8a6f790fd5a107
alinemsiqueira <aline.mscruz1@gmail.com>
2021-07-28 15:53:55 -0300

    Add variables exercises

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/variables/scripts.js
@@ -0,0 +1,14 @@
+const myName = 'Aline Siqueira'
+const birthCity = 'Jundiaí'
+let birthYear = 1994
+
+console.log(myName, birthCity, birthYear)
+
+birthYear = 2030
+
+console.log(birthYear)
+
+birthCity = 'São Paulo'
+
+// A mensagem de erro exibida no console log abaixo é por estar tentando editar uma propriedade const
+console.log(birthCity)
