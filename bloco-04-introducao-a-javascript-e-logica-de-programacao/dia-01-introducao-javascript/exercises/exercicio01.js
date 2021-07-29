0b1ed8ec2b9e8ef21d04dc6465d67237880677aa
alinemsiqueira <aline.mscruz1@gmail.com>
2021-07-28 21:43:02 -0300

    Add exercise 1

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercises/exercicio01.js
@@ -0,0 +1,21 @@
+let operator = '+'
+let a = 10
+let b = 2
+
+switch(operator){
+    case '+':
+        console.log(a + b);
+        break;
+    case '-':
+        console.log(a - b);
+        break;
+    case '*':
+        console.log(a * b);
+        break;
+    case '/':
+        console.log(a / b);
+        break;
+    case '%':
+        console.log(a % b);
+        break;
+}
