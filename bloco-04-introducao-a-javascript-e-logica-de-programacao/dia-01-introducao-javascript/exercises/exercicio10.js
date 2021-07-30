ac08ab8cc7427d24885b08b81bc689b631944a5c
alinemsiqueira <aline.mscruz1@gmail.com>
2021-07-30 07:18:10 -0300

    Add exercise 10

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercises/exercicio10.js
@@ -0,0 +1,12 @@
+let valorCustoTotal = 100 + 100 * 0.2;
+let valorVenda = 150;
+
+if (valorCustoTotal < valorVenda && valorCustoTotal > 0 && valorVenda > 0) {
+    console.log(valorVenda - valorCustoTotal);
+}
+else if (valorCustoTotal > valorVenda && valorCustoTotal > 0 && valorVenda > 0) {
+    console.log('O valor de venda é abaixo do valor de custo!');
+}
+else {
+    console.log('Erro! Os valores não podem ser zero!');
+}
