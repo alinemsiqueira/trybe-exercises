99ec70018be51875941671c04a1d3e7c96b08251
alinemsiqueira <aline.mscruz1@gmail.com>
2021-07-30 07:45:09 -0300

    Add exercise 11

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercises/exercicio11.js
@@ -0,0 +1,39 @@
+let salario = 5300.00;
+
+if (salario <= 1556.94) {
+    salario = salario * 0.92;
+    console.log(salario);
+}
+else if (salario >= 1556.95 && salario <= 2594.92) {
+    if (salario < 1903.99){
+        salario = (salario * 0.92)
+        console.log(salario);
+    }
+    else if (salario >= 1903.99 && salario <= 2826.65) {
+        salario = (salario * 0.91) - ((salario * 0.925) - 142.80);
+        console.log(salario);
+    }
+}
+else if (salario >= 2594.93 && salario <= 5189.82) {
+    if (salario >= 1903.99 && salario <= 2826.65) {
+        salario = salario * 0.89 - ((salario * 0.925) - 142.80);
+        console.log(salario);
+    }
+    else if (salario >= 2826.66 && salario <= 3751.05) {
+        salario = salario * 0.89 - ((salario * 0.85) - 354.80);
+        console.log(salario);
+    }
+    else if (salario >= 3571.06 && 4664.68) {
+        salario = salario * 0.89 - ((salario * 0.775) - 636.13);
+        console.log(salario);
+    }
+    else if (salario > 4664.68) {
+        salario = salario * 0.89 - ((salario * 0.725) - 869.36);
+        console.log(salario);
+    }
+}
+else if (salario > 5189.82){
+    salario = salario - 570.88;
+    salario = (salario * 0.725) - 869.36;
+    console.log(salario);
+}
