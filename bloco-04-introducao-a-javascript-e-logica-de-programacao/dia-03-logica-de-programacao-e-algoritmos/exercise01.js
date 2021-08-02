6a3f2fa459f4886eabe01f7e0306f2ed64b3891c
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-02 14:28:32 -0300

    Add exercise 01

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercise01.js
@@ -0,0 +1,12 @@
+let tamanhoBase = 5;
+
+for (let index = 0; index < tamanhoBase; index === tamanhoBase) {
+  index += 1;
+  let result = "*";
+
+  for(let star = tamanhoBase; star === tamanhoBase; star += 1)
+    result += '*';
+  console.log(result)
+  }
+
+
