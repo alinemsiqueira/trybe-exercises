13e61b43145d9f056c72b038a782de263f1e81fa
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-10 00:38:43 -0300

    Fix exercise01 logic

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercise01.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercise01.js
@@ -1,12 +1,9 @@
 let tamanhoBase = 5;
 
-for (let index = 0; index < tamanhoBase; index === tamanhoBase) {
-  index += 1;
-  let result = "*";
-
-  for(let star = tamanhoBase; star === tamanhoBase; star += 1)
+for (let index = 1; index <= tamanhoBase; index += 1) {
+  let result = ''
+  for(let star = 1; star <= tamanhoBase; star += 1) {
     result += '*';
-  console.log(result)
   }
-
-
+  console.log(result)
+}
