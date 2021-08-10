2cc92ca2369f8ec8fb4074076d4b0a14243aaa90
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-10 00:56:30 -0300

    Add exercise04

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercise04.js
@@ -0,0 +1,12 @@
+let n = 4;
+
+for (let index = 1; index <= n; index += 1) {
+  let result = '';
+  for(let spaces = 1; spaces <= (n - index); spaces += 1) {
+    result += ' ';
+  }
+  for(let star = 1; star <= index; star += 1) {
+    result += "* ";
+  }
+  console.log(result);
+}
