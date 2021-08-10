2e7266ebdbc3fe2c0c99983d87288a11abe4a1c6
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-10 00:56:02 -0300

    Fix apostrophes

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercise01.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercise01.js
@@ -1,8 +1,8 @@
-let tamanhoBase = 5;
+let n = 5;
 
-for (let index = 1; index <= tamanhoBase; index += 1) {
+for (let index = 1; index <= n; index += 1) {
   let result = ''
-  for(let star = 1; star <= tamanhoBase; star += 1) {
+  for(let star = 1; star <= n; star += 1) {
     result += '*';
   }
   console.log(result)
