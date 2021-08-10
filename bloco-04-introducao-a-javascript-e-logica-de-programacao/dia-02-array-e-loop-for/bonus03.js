2f2d206238b67b5a2515db2ebeab7a1b6e13db61
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-10 00:16:23 -0300

    Add bonus03

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/bonus03.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/bonus03.js
@@ -1,9 +1,11 @@
-for (let index = 1; index < array.length; index += 1) {
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (array[index] < array[secondIndex]) {
-      let position = array[index];
-      array[index] = array[secondIndex];
-      array[secondIndex] = position;
-    }
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multiply = [];
+
+for (let count = 0; count < numbers.length; count += 1) {
+  if (numbers[count + 1] === undefined) {
+    multiply.push(numbers[count] * 2);
+  } else {
+    multiply.push(numbers[count] * numbers[count + 1]);
   }
 }
+console.log(multiply);
