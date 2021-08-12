517bcbccc4bbcfcab250f1f1dfb8a33fbc2de03e
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-12 00:35:19 -0300

    Add objects exercise 05

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/exercises-objects-and-for-in.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/exercises-objects-and-for-in.js
@@ -10,12 +10,29 @@ console.log('Bem-vinda, ' + info.personagem);
 // Exercício 02
 info['recorrente'] = 'Sim';
 
-// Exercício 03
+// // Exercício 03
 for (let key in info) {
   console.log(key);
 }
 
-// Exercício 04
+// // Exercício 04
 for (let key in info) {
   console.log(info[key]);
 }
+
+// Exercício 05
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info) {
+  if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+    console.log('Ambos recorrentes');
+    break;
+  }
+  console.log(info[key] + ' e ' + info2[key]);
+}
+
