fc2f1276a1fd0da2471caf75164c87a62ddd08ea
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-11 22:31:46 -0300

    Update exercise 04 logic

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercise04.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercise04.js
@@ -1,12 +1,19 @@
-let n = 4;
+let n = 7;
 
-for (let index = 1; index <= n; index += 1) {
+for (let lines = 1; lines <= n; lines += 2) {
   let result = '';
-  for(let spaces = 1; spaces <= (n - index); spaces += 1) {
-    result += ' ';
+  let spacing = (n - lines) / 2
+
+  for (let space = spacing; space > 0; space -= 1) {
+      result += ' ';
   }
-  for(let star = 1; star <= index; star += 1) {
-    result += '* ';
+
+  for (let star = 1; star <= lines; star += 1) {
+      result += '*'
+  }
+
+  for (let space = spacing; space > 0; space -= 1) {
+    result += ' ';
   }
   console.log(result);
 }
