3954677f5a5e9411c0b9cebfb3c845aaad93b352
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-11 22:19:36 -0300

    Add exercise 11

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercise11.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercise11.js
@@ -1,39 +1,29 @@
-let salario = 2300.00;
-
-if (salario <= 1556.94) {
-    salario = salario * 0.92;
-    console.log(salario);
-}
-else if (salario >= 1556.95 && salario <= 2594.92) {
-    if (salario < 1903.99){
-        salario = (salario * 0.92)
-        console.log(salario);
-    }
-    else if (salario >= 1903.99 && salario <= 2826.65) {
-        salario = (salario * 0.91) - ((salario * 0.925) - 142.80);
-        console.log(salario);
-    }
-}
-else if (salario >= 2594.93 && salario <= 5189.82) {
-    if (salario >= 1903.99 && salario <= 2826.65) {
-        salario = salario * 0.89 - ((salario * 0.925) - 142.80);
-        console.log(salario);
-    }
-    else if (salario >= 2826.66 && salario <= 3751.05) {
-        salario = salario * 0.89 - ((salario * 0.85) - 354.80);
-        console.log(salario);
-    }
-    else if (salario >= 3571.06 && 4664.68) {
-        salario = salario * 0.89 - ((salario * 0.775) - 636.13);
-        console.log(salario);
-    }
-    else if (salario > 4664.68) {
-        salario = salario * 0.89 - ((salario * 0.725) - 869.36);
-        console.log(salario);
-    }
-}
-else if (salario > 5189.82){
-    salario = salario - 570.88;
-    salario = (salario * 0.725) - 869.36;
-    console.log(salario);
-}
+  let salario = 3000.00;
+  let salarioLiquido = 0;
+// INSS
+  if (salario < 1556.94) {
+    salarioLiquido = salario - (salario * 8) / 100;
+  } else if (salario <= 2594.92) {
+    salarioLiquido = salario - (salario * 7.5) / 100;
+  } else if (salario <= 5189.82) {
+    salarioLiquido = salario - (salario * 11) / 100;
+  } else if (salario > 5189.82) {
+    salarioLiquido = salario - 570.88;
+  }
+  
+// IR (Imposto de Renda)
+  if (salarioLiquido < 1903.98) {
+    console.log(salarioLiquido);
+  } else if (salarioLiquido <= 2826.65) {
+    salarioLiquido = salarioLiquido - ((salarioLiquido * 7.5) / 100 - 142.80);
+    console.log(salarioLiquido);
+  } else if (salarioLiquido <= 3751.05) {
+    salarioLiquido = salarioLiquido - ((salarioLiquido * 15) / 100 - 354.80);
+    console.log(salarioLiquido);
+  } else if (salarioLiquido <= 4664.68) {
+    salarioLiquido = salarioLiquido - ((salarioLiquido * 22.5) / 100 - 636.13);
+    console.log(salarioLiquido);
+  } else if (salario > 4664.68) {
+    salarioLiquido = salarioLiquido - ((salarioLiquido * 27.5) / 100 - 869.36);
+    console.log(salarioLiquido);
+  }
