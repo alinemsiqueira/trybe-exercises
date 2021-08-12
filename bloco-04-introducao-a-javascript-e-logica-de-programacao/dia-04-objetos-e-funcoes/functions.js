85ec066209d6b05983a605eba34efe8fd97ff5c0
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-11 22:23:11 -0300

    Add exercise 11

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/functions.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/functions.js
@@ -154,3 +154,35 @@ function valorLucro (valorCusto, valorVenda) {
 }
 console.log(valorLucro(100, 150));
 
+// Exercício 11
+function calcSalario (salario) {
+  let salarioLiquido = 0;
+  // INSS
+    if (salario < 1556.94) {
+      salarioLiquido = salario - (salario * 8) / 100;
+    } else if (salario <= 2594.92) {
+      salarioLiquido = salario - (salario * 7.5) / 100;
+    } else if (salario <= 5189.82) {
+      salarioLiquido = salario - (salario * 11) / 100;
+    } else if (salario > 5189.82) {
+      salarioLiquido = salario - 570.88;
+    }
+    
+  // IR (Imposto de Renda)
+    if (salarioLiquido < 1903.98) {
+      return salarioLiquido;
+    } else if (salarioLiquido <= 2826.65) {
+      salarioLiquido = salarioLiquido - ((salarioLiquido * 7.5) / 100 - 142.80);
+      return salarioLiquido;
+    } else if (salarioLiquido <= 3751.05) {
+      salarioLiquido = salarioLiquido - ((salarioLiquido * 15) / 100 - 354.80);
+      return salarioLiquido;
+    } else if (salarioLiquido <= 4664.68) {
+      salarioLiquido = salarioLiquido - ((salarioLiquido * 22.5) / 100 - 636.13);
+      return salarioLiquido;
+    } else if (salario > 4664.68) {
+      salarioLiquido = salarioLiquido - ((salarioLiquido * 27.5) / 100 - 869.36);
+      return salarioLiquido;
+    }
+}
+console.log(calcSalario(3000.00));
