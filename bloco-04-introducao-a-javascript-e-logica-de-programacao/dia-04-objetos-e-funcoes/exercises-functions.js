5f71bf974bdd05841f17c5f3abe8ae9a009abb01
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-12 12:28:25 -0300

    Revert "Add functions exercises 02"
    This reverts commit 29bcadc70039c89760a7c461df444e75eef3bbc6.

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/exercises-functions.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/exercises-functions.js
@@ -10,17 +10,3 @@ function checkPalindrome (a) {
 console.log(checkPalindrome('arara'));
 console.log(checkPalindrome('desenvolvimento'));
 
-// Exercício 02
-function indexOfBiggerValue(a) {
-  let biggerValue = a[0];
-  let indexOfBiggerValue = 0;
-  for (var count = 1; count < a.length; count++) {
-    if (a[count] > biggerValue) {
-      indexOfBiggerValue = count;
-      biggerValue = a[count];
-    }
-  }
-  return indexOfBiggerValue;
-}
-console.log(indexOfBiggerValue([2, 3, 6, 7, 10, 1]));
-
