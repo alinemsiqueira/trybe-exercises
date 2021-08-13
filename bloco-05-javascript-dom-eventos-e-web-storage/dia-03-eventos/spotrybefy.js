b1ec5a4573c98781e7db8d6ab804d40f226dfe24
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-13 14:09:44 -0300

    Add exercise Spotrybefy 01

--- bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/spotrybefy.js
+++ bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/spotrybefy.js
@@ -5,23 +5,23 @@ const input = document.getElementById( 'input' );
 const myWebpage = document.getElementById( 'my-spotrybefy' );
 
 
-  // Copie esse arquivo e edite apenas ele;
-  // Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-  // 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-let tech = document.createElement('#tech');
-//   Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
-
-  // Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 1. Que tal redirecionar para seu portifólio?
-function dblClickTitle (event) {
-  
+// Copie esse arquivo e edite apenas ele;
+// 1. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+// * Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function techClass ( event ) {
+  let tech = document.querySelector( '.tech' );
+  tech.classList.remove( 'tech' );
+  event.target.className = 'tech';
 }
+firstLi.parentElement.addEventListener( 'click', techClass );
 
-// Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+//  2. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+// 3. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// * Que tal redirecionar para seu portifólio?
+// 4. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-
 // Segue abaixo um exemplo do uso de event.target:
 
 function resetText ( event ) {
