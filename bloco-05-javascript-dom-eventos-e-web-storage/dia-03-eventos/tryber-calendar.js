9370f015909f717fb91be87f8b9e299bc0ec1b25
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-21 05:18:46 -0300

    Add exercise 01

---
+++ bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/tryber-calendar.js
@@ -0,0 +1,50 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+  
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+    
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+const buttonContainer = document.querySelector('button-container');
+createFridayButton(); // Parte do exercício 05, posicionado aqui apenas para melhor estética.
+createHolidayButton(); // Parte do exercício 02, posicionado aqui apenas para melhor estética.
+// Exercício 01
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDays() {
+  for (let day = 0; day < dezDaysList.length; day += 1) {
+    const createLi = document.createElement('li');
+    createLi.innerText = dezDaysList[day];
+    createLi.classList.add('days');
+    document.querySelector('#days').appendChild(createLi);
+  }
+}
+createDays();
+
+function addFridays() {
+  const friday = document.querySelectorAll('.days');
+  for (let day = 0; day < friday.length; day += 1) {
+    if (friday[day].innerText === '4' || friday[day].innerText === '11' || friday[day].innerText === '18' || friday[day].innerText === '25') {
+      friday[day].classList.add('friday');
+    }
+  }
+}
+addFridays();
+function addHolidays() {
+  const holiday = document.querySelectorAll('.days');
+  for (let day = 0; day < holiday.length; day += 1) {
+    if (holiday[day].innerText === '24' || holiday[day].innerText === '25' || holiday[day].innerText === '31') {
+      holiday[day].classList.add('holiday');
+    }
+  }
+}
+addHolidays();
