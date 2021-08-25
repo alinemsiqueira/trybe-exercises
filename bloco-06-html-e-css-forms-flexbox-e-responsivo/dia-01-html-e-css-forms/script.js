e21523694b1b0e5d5d5446f0ea4f06604696b8fa
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-25 08:49:47 -0300

    Add form exercise

---
+++ bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -0,0 +1,37 @@
+const estado = document.querySelector('#estado-select');
+let todosOsEstados = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goías',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraíma',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins'
+};
+const quantosEstados = Object.keys(todosOsEstados).length;
+
+for ( let count = 0; count < quantosEstados; count += 1) {
+  const createOption = document.createElement('option');
+  createOption.innerText = Object.keys(todosOsEstados)[count];
+  estado.appendChild(createOption);
+}
