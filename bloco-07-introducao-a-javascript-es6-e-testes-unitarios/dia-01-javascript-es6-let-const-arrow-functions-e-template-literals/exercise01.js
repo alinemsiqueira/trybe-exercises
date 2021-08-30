56f63d6ea4ce593429844bb539e57e5244df901c
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-30 13:18:35 -0300

    Add exercise 02 and bonus - part 01

--- bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercise01.js
+++ bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercise01.js
@@ -7,5 +7,19 @@ console.log(testingScope(false));
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
+const reorderOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+  return oddsAndEvens;
+}
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+const reordenedOddAndEvens = reorderOddsAndEvens();
+
+console.log(`Os números ${reordenedOddAndEvens} se encontram ordenados de forma crescente !`);
+
+// Exercício 02 - Bônus
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente !`);
