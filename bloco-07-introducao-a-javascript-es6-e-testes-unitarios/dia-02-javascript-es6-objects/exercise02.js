9a10b20a506d828723a670ac433608ff6d05e4f1
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-14 15:21:26 -0300

    Add exercise 02 complete

--- bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/exercise02.js
+++ bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/exercise02.js
@@ -18,11 +18,52 @@ const lesson3 = {
   turno: 'noite',
 };
 
+// Exercício 01
 const addNightShift = (object, key, value) => object[key] = value;
 
 addNightShift(lesson2, 'turno', 'noite');
 console.log(lesson2);
 
+// Exercício 02
 const showKeys = object => Object.keys(object);
 
 console.log(showKeys(lesson1));
+
+// Exercício 03
+const objectSize = object => Object.keys(object).length;
+
+console.log(objectSize(lesson3));
+
+// Exercício 04
+const objectValues = object => Object.values(object);
+
+// console.log(objectValues(lesson1));
+
+// Exercício 05
+const lessons = { lesson1, lesson2, lesson3 }
+const allLessons = Object.assign({}, lessons);
+
+console.log(allLessons);
+
+// Exercício 06
+const totalEstudantes = lesson1.numeroEstudantes + lesson2.numeroEstudantes + lesson3.numeroEstudantes;
+
+console.log(totalEstudantes);
+
+// Exercício 07
+const getValueByNumber = (object, index) => {
+  console.log(Object.values(object)[index]);
+}
+
+getValueByNumber(lesson1, 0);
+
+// Exercício 08
+const verifyPair = (obj, key, value) => {
+  return Object.entries(obj).some(([currKey, currValue]) => currKey === key && currValue === value);
+}
+
+console.log(Object.entries(lesson3));
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+// Output: true,
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+// Output: false
