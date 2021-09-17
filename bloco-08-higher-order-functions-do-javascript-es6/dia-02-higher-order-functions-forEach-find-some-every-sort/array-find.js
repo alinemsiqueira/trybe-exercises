97c18521f20a1e8454f8d606cc7e200fb1937784
alinemsiqueira <aline.mscruz1@gmail.com>
2021-09-17 18:24:21 -0300

    Add find exercises

---
+++ bloco-08-higher-order-functions-do-javascript-es6/dia-02-higher-order-functions-forEach-find-some-every-sort/array-find.js
@@ -0,0 +1,36 @@
+// Exercício 01
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (arr) => {
+  // Adiciona seu código aqui
+  return arr.find((number) => number % 3 === 0 && number % 5 === 0);
+}
+
+console.log(findDivisibleBy3And5(numbers))
+
+// Exercício 02
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (arr) => {
+  // Adicione seu código aqui:
+  return arr.find((name) => name.length === 5);
+}
+
+console.log(findNameWithFiveLetters(names));
+
+// Exercício 03
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  // Adicione seu código aqui
+  return musicas.find(musica => musica.id === id);
+}
+
+console.log(findMusic('31031685'));
